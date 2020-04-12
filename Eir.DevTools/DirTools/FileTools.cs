e077b1ab731af067052a96d60de313a5122f5fd0
ROBIN\Kurt <KurtA@Gjoll.com>
2020-04-12 11:22:43 -0500

    Added SaveModified

--- Eir.DevTools/DirTools/FileTools.cs
+++ Eir.DevTools/DirTools/FileTools.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using System.Text;
 using System.Threading;
 
@@ -8,6 +9,23 @@ namespace Eir.DevTools
 {
     public static class FileTools
     {
+        public static bool WriteModifiedText(String path, String text)
+        {
+            bool SaveText()
+            {
+                if (File.Exists(path) == false)
+                    return true;
+                String originalText = File.ReadAllText(path);
+                return String.CompareOrdinal(text, originalText) == 0;
+            }
+
+            if (SaveText() == false)
+                return false;
+
+            WriteAllText(path, text);
+            return true;
+        }
+
         public static void WriteAllText(String path, String text)
         {
             for (Int32 i = 0; i < 5; i++)
