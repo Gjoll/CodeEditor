0bd9d0ff7b0a9f08b31521e8a9ec524fc483cdbf
Kurt W. Allen <KurtA@Gjoll.com>
2020-06-23 15:02:44 -0500

    .

--- Eir.DevTools/Extensions/StringExtensions.cs
+++ Eir.DevTools/Extensions/StringExtensions.cs
@@ -7,6 +7,18 @@ namespace Eir.DevTools
 {
     public static class StringExtensions
     {
+        public static bool SameUrl(String url1, String url2)
+        {
+            url1 = url1.ToUpper().Trim();
+            url2 = url2.ToUpper().Trim();
+            return String.Compare(url1, url2) == 0;
+        }
+
+        public static bool UrlStartsWith(String url1, String url2)
+        {
+            return url1.StartsWith(url2, StringComparison.OrdinalIgnoreCase);
+        }
+
         public static String ToMarkup(this String[] lines)
         {
             if (lines is null)
