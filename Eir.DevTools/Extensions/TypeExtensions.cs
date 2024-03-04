aefa3d5996e47a6a280cf632ad4c53839d4c6fe4
Kurt W. Allen <KurtA@Gjoll.com>
2024-03-04 17:10:06 -0600

    .

--- Eir.DevTools/Extensions/TypeExtensions.cs
+++ Eir.DevTools/Extensions/TypeExtensions.cs
@@ -9,35 +9,14 @@ namespace Eir.DevTools
 {
     public static class TypeExtensions
     {
+        static NullabilityInfoContext nullabilityContext = new NullabilityInfoContext();
+
         static bool IsType(this Type t, String typeName)
         {
             if (t is null)
                 throw new ArgumentNullException(nameof(t));
             return t.Name == typeName;
         }
-
-        private static Dictionary<Type, string> simpleTypes = new Dictionary<Type, string>
-        {
-            {typeof(bool), "Boolean"},
-            {typeof(byte), "Byte"},
-            {typeof(sbyte), "SByte"},
-            {typeof(char), "Char"},
-            {typeof(decimal), "Decimal"},
-            {typeof(double), "Double"},
-            {typeof(float), "Single"},
-            {typeof(int), "Int32"},
-            {typeof(uint), "UInt32"},
-            {typeof(long), "Int64"},
-            {typeof(ulong), "UInt64"},
-            {typeof(object), "Object"},
-            {typeof(short), "Int16"},
-            {typeof(ushort), "UInt16"},
-            {typeof(string), "String"},
-            {typeof(DateTime), "DateTime"},
-            {typeof(DateOnly), "DateOnly"},
-            {typeof(TimeOnly), "TimeOnly"},
-            {typeof(Guid), "Guid"}
-        };
 
         static String CleanName(String name)
         {
@@ -50,24 +29,26 @@ namespace Eir.DevTools
         public static bool IsList(this Type t) => t.IsType("List`1");
         public static bool IsCode(this Type t) => t.IsType("Code`1");
         public static bool IsNullable(this Type t) => t.IsType("Nullable`1");
-        public static bool IsSimple(this Type t)
+
+        public static bool IsNullable(this PropertyInfo p)
         {
-            Type underlyingType = Nullable.GetUnderlyingType(t);
-            if (underlyingType != null)
-                t = underlyingType;
-            return simpleTypes.ContainsKey(t);
+            var nullabilityInfo = nullabilityContext.Create(p);
+            return nullabilityInfo.WriteState is NullabilityState.Nullable;
         }
 
         public static String Default(this Type t)
         {
-            Type underlyingType = Nullable.GetUnderlyingType(t);
-            if (underlyingType != null)
-                return "null";
-            if (!t.IsSimple())
-                return "null";
             if (t == typeof(string))
                 return "String.Empty";
             return "default";
+        }
+
+        public static String Default(this PropertyInfo p)
+        {
+            var nullabilityInfo = nullabilityContext.Create(p);
+            if (nullabilityInfo.WriteState is NullabilityState.Nullable)
+                return "null";
+            return p.PropertyType.Default();
         }
 
         public static string FriendlyName(this Type type)
@@ -81,9 +62,6 @@ namespace Eir.DevTools
                 return type.GetFriendlyNameOfGenericType();
             if (type.IsPointer)
                 return type.GetFriendlyNameOfPointerType();
-            var aliasName = default(string);
-            if (simpleTypes.TryGetValue(type, out aliasName))
-                return aliasName;
             String name = CleanName(type.Name);
             if (type.DeclaringType == null)
                 return name;
